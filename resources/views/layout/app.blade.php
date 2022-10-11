a4d571c0a6f39d7bb7791fa68e2e0b63406a7011
Diopranata01 <bagusdio01@gmail.com>
2022-10-11 15:06:16 +0800

    Commit new changes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,14 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 
     @yield('content')
-
 
 </body>
 </html>
