b8fd507539105e2a7a5c9a233503700eef14a144
Diopranata01 <bagusdio01@gmail.com>
2022-10-17 11:27:51 +0800

    new changes in main_testing1

--- app/Repository/MahasiswaRepository.php
+++ app/Repository/MahasiswaRepository.php
@@ -7,26 +7,10 @@ interface MahasiswaRepository{
 
     public function createMahasiswa($data);
 
-<<<<<<< HEAD
-        //changes 1, get the db into the repository Mahasiswa Model
-        return Mahasiswa::latest()-> get();
-    }
-
-    public function createMahasiswa(array $data){
-
-        // bisa sprt ini $mahasiswa = new Mahasiswa();
-        Mahasiswa::insert([
-            'nim' => $data['nim'], //request dari store function
-            'name' => $data['name'],
-            'fakultas' => $data['fakultas']
-        ]);
-    }
-=======
     public function get($id);
     
     public function editMahasiswa($id, $data);
     
     public function deleteMahasiswa($id);
     
->>>>>>> main_testing2
 }
