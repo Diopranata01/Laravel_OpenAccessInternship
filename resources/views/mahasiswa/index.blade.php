6d37b4d34cb7668a9226d07dd7aafb0c32b55628
Diopranata01 <bagusdio01@gmail.com>
2022-10-17 14:47:12 +0800

    Commit new changes to divine main

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -70,17 +70,6 @@
         <!-- START DATA -->
         <div class="my-3 p-3 m-3 bg-body rounded shadow-sm">
                 <!-- FORM PENCARIAN -->
-                {{-- <div class="pb-3">
-                  <form class="d-flex" action="" method="get">
-                      <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan kata kunci" aria-label="Search">
-                      <button class="btn btn-secondary" type="submit">Cari</button>
-                  </form>
-                </div> --}}
-                
-                <!-- TOMBOL TAMBAH DATA -->
-                {{--<div class="pb-3">
-                  <a href='' class="btn btn-primary">+ Tambah Data</a>
-                </div> --}}
           
                 <table class="table table-striped">
                     <div class="card-header text-dark border-3 border-bottom mb-4 mt-2">
